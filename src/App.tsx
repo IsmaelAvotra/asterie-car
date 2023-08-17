2898062ff4d99a90d3fe26102df13f667e297f47
AvotraIsmael <avotraismael4@gmail.com>
2023-08-17 18:07:38 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+function App() {
+  return (
+    <div className='container'>
+      <div className='left'>
+        <div className='logo'>
+          <img src='/logo.png' alt='logo asterie car' />
+        </div>
+        <div className='text'>
+          <h2>ASTERIA CAR</h2>
+          <p>Location de voiture</p>
+        </div>
+      </div>
+      <div className='right'>
+        <div className='logo'>
+          <img src='/logo.png' alt='logo asterie car' />
+        </div>
+        <div className='text'>
+          <h2>ASTERIA CAR</h2>
+          <p>Services</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
